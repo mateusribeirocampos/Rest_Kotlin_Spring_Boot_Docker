0c42632b781be7e36cec0dd1f00d737389b2b047
mateusribeirocampos <mateusrcampos@gmail.com>
2024-01-08 18:39:23 -0300

    01 primeiro endpoint

---
+++ 01FirstEndPoint_Rest_Kotlin_Spring_Boot_Docker/src/main/kotlin/br/com/campos/Startup.kt
@@ -0,0 +1,12 @@
+package br.com.campos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
