78e30dc1ed384430fedccc993d4e4796c278c39e
mateusribeirocampos <mateusrcampos@gmail.com>
2024-01-18 17:57:28 -0300

    playground refactored

--- 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/controller/MathController.kt
+++ 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/controller/MathController.kt
@@ -1,16 +1,21 @@
-package com.br.campos
+package com.br.campos.controller
 
+import com.br.campos.converter.NumberConverter
 import com.br.campos.exceptions.UnsupportedMathOperationException
+import com.br.campos.converter.NumberConverter.convertToDouble
+import com.br.campos.converter.NumberConverter.isNumeric
+import com.br.campos.math.simpleMath
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
-import java.lang.*
 
 @RestController
 class MathController {
 
     val counter: AtomicLong = AtomicLong()
+
+    private val math: simpleMath = simpleMath()
 
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(@PathVariable(value = "numberOne") numberOne: String?,
@@ -19,7 +24,7 @@ class MathController {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             // tratamento do erro
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return convertToDouble(numberOne) + convertToDouble(numberTwo)
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
     }
     @RequestMapping(value = ["/sub/{numberOne}/{numberTwo}"])
     fun sub(@PathVariable(value = "numberOne") numberOne: String?,
@@ -27,7 +32,7 @@ class MathController {
     ): Double {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return convertToDouble(numberOne) - convertToDouble(numberTwo)
+        return math.sub(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
     }
     @RequestMapping(value = ["/mult/{numberOne}/{numberTwo}"])
     fun mult(@PathVariable(value = "numberOne") numberOne: String?,
@@ -35,7 +40,7 @@ class MathController {
     ): Double {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return convertToDouble(numberOne) * convertToDouble(numberTwo)
+        return math.mult(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
     }
     @RequestMapping(value = ["/div/{numberOne}/{numberTwo}"])
     fun div(@PathVariable(value = "numberOne") numberOne: String?,
@@ -43,7 +48,7 @@ class MathController {
     ): Double {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return convertToDouble(numberOne) / convertToDouble(numberTwo)
+        return math.div(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
     }
     @RequestMapping(value = ["/mean/{numberOne}/{numberTwo}"])
     fun mean(@PathVariable(value = "numberOne") numberOne: String?,
@@ -51,37 +56,20 @@ class MathController {
     ): Double {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2
+        return math.mean(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
     }
     @RequestMapping(value = ["/sqrt/{numberOne}"])
     fun sqrt(@PathVariable(value = "numberOne") numberOne: String?
     ): Double {
         if(!isNumeric(numberOne))
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return kotlin.math.sqrt(convertToDouble(numberOne))
+        return math.sqrt(NumberConverter.convertToDouble(numberOne))
     }
     @RequestMapping(value = ["/log2/{numberOne}"])
     fun log2(@PathVariable(value = "numberOne") numberOne: String?
     ): Double {
         if(!isNumeric(numberOne))
             throw UnsupportedMathOperationException("Por favor, insira um número válido!\nPlease set a numeric value!")
-        return kotlin.math.log2(convertToDouble(numberOne))
+        return math.log2(NumberConverter.convertToDouble(numberOne))
     }
-
-    // Função convertToDouble para converter String em Double
-    private fun convertToDouble(strNumber: String?): Double {
-        // Se o numero inserido for nulo ou sem numbero retorna 0.0
-        if(strNumber.isNullOrBlank()) return 0.0
-        // converte a virgula em .
-        val number = strNumber.replace(",".toRegex(), ".")
-        // retorna o numero inserido
-        return if (isNumeric(number)) number.toDouble() else 0.0
-    }
-    // função para verificar se é um numero
-    private fun isNumeric(strNumber: String?): Boolean {
-        if(strNumber.isNullOrBlank()) return false
-        val number = strNumber.replace(",".toRegex(), ".")
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+".toRegex())
-    }
-
 }
---
+++ 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/converter/NumberConverter.kt
@@ -0,0 +1,19 @@
+package com.br.campos.converter
+
+object NumberConverter {
+    // Função convertToDouble para converter String em Double
+    fun convertToDouble(strNumber: String?): Double {
+        // Se o numero inserido for nulo ou sem numbero retorna 0.0
+        if(strNumber.isNullOrBlank()) return 0.0
+        // converte a virgula em .
+        val number = strNumber.replace(",".toRegex(), ".")
+        // retorna o numero inserido
+        return if (isNumeric(number)) number.toDouble() else 0.0
+    }
+    // função para verificar se é um numero
+    fun isNumeric(strNumber: String?): Boolean {
+        if(strNumber.isNullOrBlank()) return false
+        val number = strNumber.replace(",".toRegex(), ".")
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+".toRegex())
+    }
+}
