29aaee5a5304dc704ce2f7f45b7b04242a5df835
mateusribeirocampos <mateusrcampos@gmail.com>
2024-01-25 16:28:10 -0300

    código comentado

--- 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/controller/PersonController.kt
+++ 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/controller/PersonController.kt
@@ -1,3 +1,4 @@
+// Importando as classes necessárias
 package com.br.campos.controller
 
 import com.br.campos.model.Person
@@ -9,19 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
+// @RestController é uma anotação do Spring que combina a funcionalidade de @Controller e @ResponseBody.
+// Isso significa que esta classe é um controlador REST e as respostas dos métodos são automaticamente convertidas para JSON.
 @RestController
+//
+// @RequestMapping é uma anotação que mapeia URLs para métodos de controlador. Neste caso, todas as URLs começarão com "/person".
 @RequestMapping("/person")
 class PersonController {
 
+    // @Autowired é uma anotação do Spring que injeta automaticamente a instância de PersonServices.
+    // Isso significa que você não precisa criar manualmente uma instância de PersonServices, o Spring fará isso por você.
     @Autowired
     lateinit var services: PersonServices
 
+    // @RequestMapping é usado novamente para mapear a URL "/person/{id}" para este método.
+    // O método será invocado quando uma solicitação GET for feita para essa URL.
+    // O valor do parâmetro {id} é capturado usando @PathVariable.
+    // O método produzirá uma resposta JSON, conforme especificado por "produces = [MediaType.APPLICATION_JSON_VALUE]".
     @RequestMapping(
         value = ["/{id}"],
         method = [RequestMethod.GET],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
     fun findById(@PathVariable("id") id: Long): Person {
+        // O método chama services.findById(id) para obter uma instância de Person com o ID fornecido.
+        // O resultado será automaticamente convertido para JSON e retornado como resposta.
         return services.findById(id)
     }
 }
--- 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/services/PersonServices.kt
+++ 099Playground_RKSBD/099Playground_RKSBD/src/main/kotlin/com/br/campos/services/PersonServices.kt
@@ -1,3 +1,4 @@
+// Importando as classes necessárias
 package com.br.campos.services
 
 import com.br.campos.model.Person
@@ -5,24 +6,33 @@ import org.springframework.stereotype.Service
 import java.util.concurrent.atomic.AtomicLong
 import java.util.logging.Logger
 
+// @Service é uma anotação do Spring que marca essa classe como um serviço.
+// Isso significa que o Spring gerenciará automaticamente a criação e a injeção de instâncias dessa classe.
 @Service
 class PersonServices {
 
+    // Um contador atômico para gerar IDs únicos.
     private val counter: AtomicLong = AtomicLong()
 
+    // Um logger para registrar mensagens. Neste caso, está configurado para logar mensagens para a classe PersonServices.
     private val logger = Logger.getLogger(PersonServices::class.java.name)
 
+    // Método para encontrar uma pessoa pelo ID.
     fun findById(id: Long): Person {
+        // Registrando uma mensagem de log informando que está procurando uma pessoa pelo ID.
         logger.info("Find a person by id")
 
+        // Criando uma instância de Person com dados fictícios.
         val person = Person()
-        person.id = counter.incrementAndGet()
+        person.id = counter.incrementAndGet() // Aumentando o contador e atribuindo o valor como ID da pessoa.
         person.firstName = "Mateus"
         person.lastName = "Campos"
         person.birthday = "1981-01-01"
         person.age = 42
         person.address = "Rua dos Rios, 100"
         person.gender = "Male"
+
+        // Retornando a instância de Person criada.
         return person
     }
 }
