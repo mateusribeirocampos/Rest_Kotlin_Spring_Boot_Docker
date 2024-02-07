b3293b62e88d1c5a84f906022a81042d719e4dbe
mateusribeirocampos <mateusrcampos@gmail.com>
2024-02-07 16:50:04 -0300

    @Repository removed

--- 10Refactoring_Rest_Kotlin_Spring_Boot_Docker/src/main/kotlin/br/com/campos/repository/PersonRepository.kt
+++ 10Refactoring_Rest_Kotlin_Spring_Boot_Docker/src/main/kotlin/br/com/campos/repository/PersonRepository.kt
@@ -2,7 +2,5 @@ package br.com.campos.repository
 
 import br.com.campos.model.Person
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface PersonRepository : JpaRepository<Person, Long?>
