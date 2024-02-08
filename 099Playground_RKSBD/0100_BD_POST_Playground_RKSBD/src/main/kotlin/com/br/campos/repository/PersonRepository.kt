4ec96f83d888bdd1fbe4a61cb37e9539f3475145
mateusribeirocampos <mateusrcampos@gmail.com>
2024-02-08 17:38:56 -0300

    playground refactoring PersonController and PersonRepository

--- 099Playground_RKSBD/0100_BD_POST_Playground_RKSBD/src/main/kotlin/com/br/campos/repository/PersonRepository.kt
+++ 099Playground_RKSBD/0100_BD_POST_Playground_RKSBD/src/main/kotlin/com/br/campos/repository/PersonRepository.kt
@@ -2,8 +2,7 @@ package com.br.campos.repository
 
 import com.br.campos.model.Person
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
+//@Repository - não é mais necessário pois a anotação @Repository já está presente na interface JpaRepository
 interface PersonRepository : JpaRepository<Person, Long?> {
 }
