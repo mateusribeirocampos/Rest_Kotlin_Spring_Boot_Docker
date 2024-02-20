96abc6ded83535b0674c2d5c4217a5ae034e97d2
mateusribeirocampos <mateusrcampos@gmail.com>
2024-02-20 10:21:16 -0300

    deleting project port

--- 099Playground_RKSBD/0102Playground_RKSBD_Refactoring/src/main/kotlin/br/com/playground2/excessoes/RecursosNaoEncontradosExcessoes.kt
+++
@@ -1,8 +0,0 @@
-package br.com.playground2.excessoes
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class RecursosNaoEncontradosExcessoes(excessoes: String?) : RuntimeException(excessoes) {
-}
--- 099Playground_RKSBD/0102Playground_RKSBD_Refactoring/src/main/kotlin/br/com/playground2/repositorio/RepositorioPessoas.kt
+++
@@ -1,7 +0,0 @@
-package br.com.playground2.repositorio
-
-import br.com.playground2.modelo.Pessoas
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RepositorioPessoas : JpaRepository<Pessoas, Long?> {
-}
