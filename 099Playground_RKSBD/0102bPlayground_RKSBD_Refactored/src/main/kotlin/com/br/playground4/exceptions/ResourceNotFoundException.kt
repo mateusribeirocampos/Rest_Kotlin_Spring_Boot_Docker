a132824d772c4bb911160cae12e1cc72b1e2308f
mateusribeirocampos <mateusrcampos@gmail.com>
2024-03-13 18:29:58 -0300

    Some advances

--- 099Playground_RKSBD/0102bPlayground_RKSBD_Refactored/src/main/kotlin/com/br/playground4/exceptions/ResourceNotFoundException.kt
+++ 099Playground_RKSBD/0102bPlayground_RKSBD_Refactored/src/main/kotlin/com/br/playground4/exceptions/ResourceNotFoundException.kt
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-class resourceNotFoundException(exception: String?): RuntimeException(exception){
+class ResourceNotFoundException(exception: String?): RuntimeException(exception){
 }
